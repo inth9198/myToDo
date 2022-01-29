0ea42ea464c046b6cc61ae5e5876509293233246
수정 <53827482+MODIFYC@users.noreply.github.com>
2022-01-30 00:01:09 +0900

    ver2
    * 작가 이름에 따옴표 추가

--- js/quotes.js
+++ js/quotes.js
@@ -1,34 +1,34 @@
 const quotes = [
 {
     quote:"The way get started is to quit talking and begin doing.",
-    author:"Walt Disney",
+    author:"\"Walt Disney\"",
 },{
     quote:"The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    author:"Nelson Mandela",
+    author:"\"Nelson Mandela\"",
 },{
     quote:"Anyone who has never made a mistake has never tried anything new.",
-    author:"Albert Einstein",
+    author:"\"Albert Einstein\"",
 },{
     quote:"Let me assert my firm belief that the only thing we have to fear is fear itself.",
-    author:"F.D. Roosevelt",
+    author:"\"F.D. Roosevelt\"",
 },{
     quote:"Age is no guarantee of maturity.",
-    author:"Lawana Blackwell",
+    author:"\"Lawana Blackwell\"",
 },{
     quote:"Sometimes even to live is an act of courage.",
-    author:"Seneca",
+    author:"\"Seneca\"",
 },{
     quote:"There is no cure for birth and death, save to enjoy the interval.",
-    author:"George Santayana",
+    author:"\"George Santayana\"",
 },{
     quote:"By nature, men are nearly alike. by practice, they get to be wide apart.",
-    author:"Confucius",
+    author:"\"Confucius\"",
 },{
     quote:"Science is organized knowledge. Wisdom is organized life.",
-    author:"Immanuel Kant",
+    author:"\"Immanuel Kant\"",
 },{
     quote:"ife is not fair get used to it.",
-    author:"Bill Gates",
+    author:"\"Bill Gates\"",
 }]
 
 const quote = document.querySelector("#quote span:first-child");
@@ -36,5 +36,6 @@ const author = document.querySelector("#quote span:last-child");
 
 const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
+author.classList.add("author");
 quote.innerText = todayQuote.quote;
 author.innerText = todayQuote.author;
