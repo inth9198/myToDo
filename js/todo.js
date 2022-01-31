2bc10828771a815554f1fbc7bb32c93b49845eee
minholee <inth2474@naver.com>
2022-01-31 21:53:30 +0900

    dday

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,6 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");
+const toDoInput = toDoForm.querySelector("#toDoInput");
+const dateInput = toDoForm.querySelector("#date");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
@@ -16,25 +17,42 @@ function deleteToDo(event){
     saveToDos();
 }
 
+function calculateDate(date){
+    const setDate = new Date(`${date}T00:00:00+0000`);
+    const now = new Date();
+    const distance = setDate.getTime() - now.getTime();
+    const day = Math.ceil(distance/(1000*60*60*24));
+    return day;
+}
+
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText=newTodo.text;
+    span.innerText = newTodo.text;
+    const dateSpan = document.createElement("span");
+    const nDate = calculateDate(newTodo.date);
+    dateSpan.innerText=" D-day " + nDate;
     const button = document.createElement("button");
     button.innerText="❌";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
+    li.appendChild(dateSpan);
     li.appendChild(button);
+
     toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
+    const newDate = dateInput.value;
     toDoInput.value = "";
+    dateInput.value = "";
     const newToDoObj = {
         text: newTodo,
+        date: newDate,
         id: Date.now(),
     }
     toDos.push(newToDoObj);
